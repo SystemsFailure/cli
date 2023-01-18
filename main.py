654691a4eb9d3cd32734c1945a14dbdc6dfd00b4
SystemFailure <allistirking422@gmail.com>
2023-01-18 05:11:21 -0900

    00.3update2023

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import typer
+from rich import print
 app = typer.Typer()
 
 
@@ -8,8 +9,13 @@ def download(url: str):
         print(url)
 
 @app.command()
-def upload(url: str, globalpath: int):
-    print('no')
-
+def upload(url: str, globalpath: str = '', formal: bool = False):
+    """
+    This is documentation from lovela! :)
+    """
+    if formal:
+        print('[bold cyan2]Access[/bold cyan2] : ' + 'succesful upload from : {}'.format(url))
+    else:
+        print('[bold red]Error[/bold red] : ' + 'not access. Wheare --formal? write --formal - txt')
 if __name__ == '__main__':
     app()
